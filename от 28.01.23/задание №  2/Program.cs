dc27d57640d4a128d94f4218c82dc378809c2e7c
alikc <0pnessst@gmail.com>
2023-01-28 16:28:41 +0300

    task four complated hw28.01.23

--- от 28.01.23/задание №  2/Program.cs
+++ от 28.01.23/задание №  2/Program.cs
@@ -1,1 +1,18 @@
 ﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+
+Console.WriteLine("введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max = {num1} > min = {num2}");
+}
+else
+{
+    Console.WriteLine($"max = {num2} > min = {num1}");
+}
--- от 28.01.23/задание №  4/Program.cs
+++ от 28.01.23/задание №  4/Program.cs
@@ -1,1 +1,30 @@
 ﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+
+Console.WriteLine("введите первое число");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите второе число");
+int num2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("введите второе число");
+int num3 = int.Parse(Console.ReadLine()!);
+ int max = num1;
+ if (num1 < num2)
+ {
+    max = num2;
+ }
+ else
+ {
+
+ }
+ if (max < num3)
+ {
+    max = num3;
+ }
+else
+{
+
+}
+ Console.WriteLine($"самое большое число {max}");
